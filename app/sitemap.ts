f170bccbbc6dd77ca1e502f36a264d2d694b97d3
laxman200456 <laxmancsuthar2004@gmail.com>
2025-10-05 15:25:19 +0530

    sitemap updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -33,7 +33,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
       changeFrequency: 'monthly' as const,
-      priority: 0.7,
+      priority: 0.9,
     },
     
   ]
